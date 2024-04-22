0c95bdd617a145333dc1d2e54a04d141a75b43bd
James Rhodes <james21198@gmail.com>
2024-04-22 17:31:35 +0100

    Models Index JS File Updated

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,30 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     // define columns
   },
   {
